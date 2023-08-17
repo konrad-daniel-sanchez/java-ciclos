45e96b30647901181a2a183598af12153cc9af58
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-17 01:43:42 -0500

    Identificar si un número es primo.

---
+++ src/NumeroPrimo.java
@@ -0,0 +1,25 @@
+// Programa que identifique si un número es primo
+import java.util.Scanner;
+
+public class NumeroPrimo {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("PROGRAMA QUE DETERMINA SI UN NÚMERO ES PRIMO");
+        System.out.println("Ingresa el número");
+        int numero = lector.nextInt();
+        boolean esPrimo = true;
+        int i = 2;
+        while(i < numero){
+            if(numero % i == 0){
+                esPrimo = false;
+            }
+            i++;
+        }
+        if(esPrimo) {
+            System.out.println("Sí es primo");
+        }
+        else {
+            System.out.println("No es primo");
+        }
+    }
+}

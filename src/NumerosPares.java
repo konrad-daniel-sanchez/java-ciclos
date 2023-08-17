df626d4a260e425831d2f7b3cee82e74db0c9590
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-17 00:16:23 -0500

    Se adiciona la impresión de los números pares menores que 100.

---
+++ src/NumerosPares.java
@@ -0,0 +1,8 @@
+// Imprimir los números pares menores que 100
+public class NumerosPares {
+    public static void main(String[] args) {
+        for(int i = 0; i< 100; i+=2){
+            System.out.println(i);
+        }
+    }
+}

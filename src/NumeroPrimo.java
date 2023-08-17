bb7dc60c70fa9fe65299cf27ec9e4f803abb6047
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-17 02:45:47 -0500

    Se optimiza el cálculo del primo, dividiendo solamente hasta la raíz de N.

--- src/NumeroPrimo.java
+++ src/NumeroPrimo.java
@@ -9,7 +9,7 @@ public class NumeroPrimo {
         int numero = lector.nextInt();
         boolean esPrimo = true;
         int i = 2;
-        while(i < numero && esPrimo == true){
+        while(i < Math.sqrt(numero)){
             if(numero % i == 0){
                 esPrimo = false;
             }

5a5978f34bd77f0cc74f01bfe6deb3f747be4fa1
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-17 00:05:09 -0500

    Primera versión, imprimiendo los primeros 99 números naturales.

---
+++ src/NumerosNaturales.java
@@ -0,0 +1,8 @@
+// Imprimir los primeros 99 números enteros.
+public class NumerosNaturales {
+    public static void main(String[] args) {
+        for(int i = 0; i< 100; i++){
+            System.out.println(i);
+        }
+    }
+}
